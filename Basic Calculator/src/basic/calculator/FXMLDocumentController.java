b62d584f16ef7f490796d0ff689e630610bd4d81
ebrahimkrianbd <ebrahim.k.rian.bd@gmail.com>
2016-10-07 08:34:57 +0600

    Basic Calculator

--- Basic Calculator/src/basic/calculator/FXMLDocumentController.java
+++ Basic Calculator/src/basic/calculator/FXMLDocumentController.java
@@ -22,11 +22,16 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private TextField DesplyField;
+    
+    String oldText = "";
+    String newText = "";
 
     double number1, number2;
     String operation;
     String save = "0";
+    boolean resultUpdated = true;
     boolean operationPending = true;
+    boolean mathError = true;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -35,64 +40,74 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void handleEightAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "8";
+        oldText = DesplyField.getText();
+        newText = oldText + "8";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("8");
         }
+        }
     }
 
     @FXML
     private void handleNineAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "9";
+        oldText = DesplyField.getText();
+        newText = oldText + "9";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("9");
         }
+        }
     }
 
     @FXML
     private void handleDivisionAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        
+        oldText = DesplyField.getText();
         number1 = Double.parseDouble(oldText);
         operation = "Division";
         DesplyField.setText("");
+        }
     }
 
     @FXML
     private void handleAllDeleteAction(ActionEvent event) {
         DesplyField.setBlendMode(BlendMode.SRC_OVER);
         DesplyField.setText("");
+        mathError = true;
     }
 
     @FXML
     private void handleClearAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
-        String newText = null;
+        if(mathError){
+        oldText = DesplyField.getText();
+        newText = null;
         int len = oldText.length();
         
         if(len > 0){
         newText = oldText.substring(0, len-1);
         DesplyField.setText(newText);
         }
+        }
 
-        /*String newText = "";
+        /*newText = "";
         for (int i = 0; i < len - 1; i++) {
             newText = newText + oldText.charAt(i);
         }
@@ -101,244 +116,289 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void handleFiveAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "5";
+        oldText = DesplyField.getText();
+        newText = oldText + "5";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("5");
         }
+        }
     }
 
     @FXML
     private void handleSixAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "6";
+        oldText = DesplyField.getText();
+        newText = oldText + "6";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("6");
         }
+        }
     }
 
     @FXML
     private void handleMultiplyAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         number1 = Double.parseDouble(oldText);
         operation = "Multiply";
         DesplyField.setText("");
+        }
     }
     
     @FXML
     private void handleTwoAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
           
-        if(!operationPending){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "2";
+        oldText = DesplyField.getText();
+        newText = oldText + "2";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("2");
         }
+        }
     }
 
     @FXML
-    private void handleThreeAction(ActionEvent event) { 
-        if(!operationPending){
+    private void handleThreeAction(ActionEvent event) {
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "3";
+        oldText = DesplyField.getText();
+        newText = oldText + "3";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("3");
         }
+        }
     }
 
     @FXML
     private void handleSubtructAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         number1 = Double.parseDouble(oldText);
         operation = "Subruct";
         DesplyField.setText("");
+        }
 
     }
 
     @FXML
     private void handleXSqrAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         double data = Double.parseDouble(oldText);
         double sqr = data * data;
 
         DesplyField.setText("" + sqr);
-
+        }
     }
 
     @FXML
     private void handleRootAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         double data = Double.parseDouble(oldText);
         double sqrt = Math.sqrt(data);
 
         DesplyField.setText("" + sqrt);
+        }
     }
 
     @FXML
     private void handleSevenAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "7";
+        oldText = DesplyField.getText();
+        newText = oldText + "7";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("7");
         }
+        }
     }
 
     @FXML
     private void handlePointAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         if (!oldText.contains(".")) {
-            String newText = oldText + ".";
+            newText = oldText + ".";
             DesplyField.setText(newText);
+        }
         }
     }
 
     @FXML
     private void handlePersendAction(ActionEvent event) {
+        if(mathError){
+            
+        }
     }
 
     @FXML
     private void handleEqualAction(ActionEvent event) {
-        String newText = DesplyField.getText();
+        if(mathError){
+        newText = DesplyField.getText();
+        if(newText.equals("")){
+            DesplyField.setBlendMode(BlendMode.RED);
+            DesplyField.setText("Math Error! click " +"AC" +" and continue.");
+            mathError = false;
+        }else{
         number2 = Double.parseDouble(newText);
         double result = 0.0;
         if (operation == "Add") {
             result = number1 + number2;
             DesplyField.setText("" + result);
-            operationPending = false;
+            resultUpdated = false;
         } else if (operation == "Subruct") {
             result = number1 - number2;
             DesplyField.setText("" + result);
-            operationPending = false;
+            resultUpdated = false;
         } else if (operation == "Multiply") {
             result = number1 * number2;
             DesplyField.setText("" + result);
-            operationPending = false;
+            resultUpdated = false;
         } else if (operation == "Division") {
             
             if(number2 != 0){
             result = number1 / number2;
             DesplyField.setText("" + result);
-            operationPending = false;
+            resultUpdated = false;
             }else{
                 DesplyField.setBlendMode(BlendMode.RED);
                 DesplyField.setText("Math Error! click " +"AC" +" and continue.");
+                mathError = false;
             }
+        }
+        }
         }
     }
 
     @FXML
     private void handleFourAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
             
-        String oldText = DesplyField.getText();
-        String newText = oldText + "4";
+        oldText = DesplyField.getText();
+        newText = oldText + "4";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("4");
         }
+        }
     }
 
     @FXML
     private void handleOneAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
           
-        String oldText = DesplyField.getText();
-        String newText = oldText + "1";
+        oldText = DesplyField.getText();
+        newText = oldText + "1";
         if (!oldText.equals("0")) {
 
             DesplyField.setText(newText);
         } else {
             DesplyField.setText("1");
         }
+        }
     }
 
     @FXML
     private void handleZeroAction(ActionEvent event) {
-        if(!operationPending){
+        if(mathError){
+        if(!resultUpdated){
             DesplyField.setText("");
-            operationPending =true;
+            resultUpdated =true;
         }
           
-        String oldText = DesplyField.getText();
+        oldText = DesplyField.getText();
         if (!oldText.equals("0")) {
-            String newText = oldText + "0";
+            newText = oldText + "0";
             DesplyField.setText(newText);
+        }
         }
     }
 
     @FXML
     private void handleAddAction(ActionEvent event) {
-        String oldText = DesplyField.getText();
+        if(mathError){
+        oldText = DesplyField.getText();
         number1 = Double.parseDouble(oldText);
         operation = "Add";
         DesplyField.setText("");
+        }
     }
 
     @FXML
     private void handleMemoriReturnAction(ActionEvent event) {
+        if(mathError){
         DesplyField.setText(save);
+        }
     }
 
     @FXML
     private void handleMemoriSaveAction(ActionEvent event) {
+        if(mathError){
         save = "0";
-        String oldText = DesplyField.getText();
+        oldText = DesplyField.getText();
         save = oldText;
+        }
     }
 
     @FXML
     private void handleMemoriClearAction(ActionEvent event) {
+        if(mathError){
         save = "";
+        }
     }
 
 }
